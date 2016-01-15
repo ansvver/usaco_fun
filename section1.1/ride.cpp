25ed97b692f5b14a03fca3a19ed365c74db39486
xiaohei <wangjianhui@jiwowang.com>
2016-01-15 20:25:08 +0800

    create ride.cpp

---
+++ section1.1/ride.cpp
@@ -0,0 +1,16 @@
+/*
+ID: shsfool1
+PROG: ride
+LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ofstream fout ("test.out");
+    ifstream fin ("test.in");
+
+}
