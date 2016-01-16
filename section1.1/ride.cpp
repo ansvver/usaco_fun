3c9f2cfa175256412b05dbd3cb2eb7f48895db02
= <=>
2016-01-16 15:39:28 +0800

    ride.cpp

--- section1.1/ride.cpp
+++ section1.1/ride.cpp
@@ -9,8 +9,22 @@ LANG: C++
 
 using namespace std;
 
-int main() {
-    ofstream fout ("test.out");
-    ifstream fin ("test.in");
+int calc(const string &s) {
+    int ret = 1;
+    for (int i = 0; i < (int)s.size(); ++i) {
+        ret *= s[i] - 'A' + 1;
+    }
+    return ret % 47;
+}
 
+int main() {
+    ofstream fout ("ride.out");
+    ifstream fin ("ride.in");
+    int comet_mod = 0;
+    int group_mod = 0;
+    string a, b;
+    fin >> a >> b;
+    if (calc(a) == calc(b)) fout << "GO" << endl;
+    else fout << "STAY" << endl;
+    return 0;
 }
